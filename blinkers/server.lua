9f410e34791b9e3845885cc1b8bd30279cf1833c
theAlex <95939198+thealex-br@users.noreply.github.com>
2023-11-30 17:40:48 -0300

    Delete blinkers directory

--- blinkers/server.lua
+++
@@ -1,9 +0,0 @@
-addEventHandler("onResourceStop", resourceRoot, function()
-    local vehicles = getElementsByType("vehicle")
-    for _, veh in pairs(vehicles) do
-        local data = getElementData(veh, "blinker")
-        if data then
-            removeElementData(veh, "blinker")
-        end
-    end
-end)
